81f810a68777ff0eb3cc93b341dd3947b0ff62cb
Michal Turczyn <turek1992@o2.pl>
2025-07-28 13:55:52 +0200

    Added support for multiple environments: production and dev

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        federation({
-            name: 'host_app',
-            remotes: {
-                remoteApp:
-                    'http://mf-remote.azurewebsites.net/assets/remoteEntry.js',
-            },
-            shared: ['react', 'react-dom'],
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [
+            react(),
+            federation({
+                name: 'host_app',
+                remotes: {
+                    remoteApp: env.VITE_REMOTE_URL,
+                },
+                shared: ['react', 'react-dom'],
+            }),
+        ],
+    }
 })
