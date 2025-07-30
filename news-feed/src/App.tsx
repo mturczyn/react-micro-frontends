db1b54be2f320ce9136936fe17cc5f36eb8afe4d
Michal Turczyn <turek1992@o2.pl>
2025-07-30 21:43:33 +0200

    News feed app: Deployment for dedicated path test.

--- news-feed/src/App.tsx
+++ news-feed/src/App.tsx
@@ -2,7 +2,7 @@ import { NewsFeed } from './NewsFeed'
 
 function App() {
     return (
-        <div className="min-h-screen bg-gray-50 text-gray-900">
+        <div className=" min-h-screen bg-gray-50 text-gray-900">
             <h1 className="text-2xl font-bold text-center py-6">
                 🗞️ Latest News
             </h1>
